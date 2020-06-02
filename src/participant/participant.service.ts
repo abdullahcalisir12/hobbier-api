caa5b35543e65c4fc562cb0994cabc6e22a9fd30
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-06-02 22:51:16 +0300

    participant service updated

--- src/participant/participant.service.ts
+++ src/participant/participant.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, ConflictException, BadRequestException } from '@nestjs/common';
+import { Injectable, ConflictException, BadRequestException, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Participant } from './participant.entity';
 import { Repository } from 'typeorm';
@@ -12,7 +12,7 @@ export class ParticipantService {
     private participantsRepository: Repository<Participant>,
     private readonly eventService: EventService
   ) {}
-  async sendRequestToAttendEvent(eventId, { id: userId }) {
+  async sendRequestToAttendEvent(eventId, { id: userId }): Promise<Participant> {
     const requestedEvent = await this.participantsRepository.findOne({where: {user: userId, event: eventId}});
     if (requestedEvent) {
       throw new ConflictException('You already sent request to this event before');
@@ -26,39 +26,54 @@ export class ParticipantService {
     return await this.participantsRepository.save(newParticipant);
   }
 
-  async cancelRequesstToAttendEvent(eventId, { id: userId }) {
-    const requestedEvent = await this.participantsRepository.findOne({where: {user: userId, event: eventId}});
-    if (!requestedEvent) {
-      throw new BadRequestException();
-    }
-    const uodateParticipationRequest = {
+  async cancelRequesstToAttendEvent(eventId, { id: userId }): Promise<Participant> {
+    const requestedEvent = await this.getParticipatedEvent(eventId, userId)
+    const updateParticipationRequest = {
       ...requestedEvent,
       participantType: ParticipantType.REQUEST_CANCELLED
     }
-    return await this.participantsRepository.save(uodateParticipationRequest);
+    return await this.participantsRepository.save(updateParticipationRequest);
   }
 
-  async confirmAttendRequest(eventId, { id: userId }) {
-    const requestedEvent = await this.participantsRepository.findOne({where: {user: userId, event: eventId}});
-    if (!requestedEvent) {
-      throw new BadRequestException();
-    }
-    const uodateParticipationRequest = {
+  async confirmAttendRequest(eventId, { id: userId }): Promise<Participant> {
+    const requestedEvent = await this.getParticipatedEvent(eventId, userId)
+    const updateParticipationRequest = {
       ...requestedEvent,
       participantType: ParticipantType.REQUEST_ACCEPTED
     }
-    return await this.participantsRepository.save(uodateParticipationRequest);
+    return await this.participantsRepository.save(updateParticipationRequest);
   }
 
-  async rejectAttendRequest(eventId, { id: userId }) {
+  async rejectAttendRequest(eventId, { id: userId }): Promise<Participant> {
+    const requestedEvent = await this.getParticipatedEvent(eventId, userId)
+    const updateParticipationRequest = {
+      ...requestedEvent,
+      participantType: ParticipantType.REQUEST_REJECTED
+    }
+    return await this.participantsRepository.save(updateParticipationRequest);
+  }
+
+  async getEventsAttendedByUser(userId): Promise<Participant[]> {
+    const eventsAttendedByUser = await this.participantsRepository.find({where: {user: userId}, relations: ["event"]});
+    if (!eventsAttendedByUser) {
+      throw new InternalServerErrorException();
+    }
+    return eventsAttendedByUser;
+  }
+
+  async getUsersAttendingEvent(eventId): Promise<Participant[]> {
+    const usersAttendingEvent = await this.participantsRepository.find({where: {event: eventId}, relations: ["user"]});
+    if (!usersAttendingEvent) {
+      throw new InternalServerErrorException();
+    }
+    return usersAttendingEvent;
+  }
+
+  async getParticipatedEvent(eventId, userId): Promise<Participant> {
     const requestedEvent = await this.participantsRepository.findOne({where: {user: userId, event: eventId}});
     if (!requestedEvent) {
       throw new BadRequestException();
     }
-    const uodateParticipationRequest = {
-      ...requestedEvent,
-      participantType: ParticipantType.REQUEST_REJECTED
-    }
-    return await this.participantsRepository.save(uodateParticipationRequest);
+    return requestedEvent;
   }
 }
